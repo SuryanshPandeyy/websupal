d91df2d2fa8bebf528740a34d1d69bfef05c4381
Suryanshpsurya <65591693+Suryanshpsurya@users.noreply.github.com>
2021-01-11 17:33:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ include 'conn.php';
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Document</title>
-<link rel="stylesheet" href="css/style.css" />
-<link rel="stylesheet" href="css/swiper.min.css" />
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="swiper.min.css" />
 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
 <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
 
 </head>
@@ -185,8 +185,8 @@ include 'conn.php';
     </footer>
   <!-- footer end-->
 
-  <script src="js/function.js"></script>
-  <script src="js/swiper.min.js"></script>
+  <script src="function.js"></script>
+  <script src="swiper.min.js"></script>
   <script>
     var swiper = new Swiper('.swiper-container', {
       effect: 'coverflow',
